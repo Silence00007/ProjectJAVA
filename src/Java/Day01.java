536da1248b99ef9e621c31c48f4a08aab0905002
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-06-17 15:56:24 +0530

    Second Commit

--- src/Java/Day01.java
+++ src/Java/Day01.java
@@ -2,6 +2,10 @@ package Java;
 
 public class Day01 {
     public static void main(String[] args) {
-        System.out.println("llll");
+        int a= 10;
+        int b=30;
+        int x =b;
+        int c = b*x*a;
+        System.out.println("Multiplication of all is  " + c);
     }
 }
