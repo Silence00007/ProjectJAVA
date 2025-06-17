d87c0e5ee5b333e3f06733435480b4ee511e6bcf
Sudhanshu Kumar <sudhanshuqa007@gmail.com>
2025-06-17 16:36:37 +0530

    created new class name day02

---
+++ src/Java/Day02.java
@@ -0,0 +1,16 @@
+package Java;
+
+public class Day02 {
+    public static void main(String[] args) {
+        int x=48;
+
+        if (x>=48)
+        {
+            System.out.println("Welcome you are Enter into the Senior Citizen");
+        }
+
+        else {
+            System.out.println("Welcome my dear you are an energetic young man");
+        }
+    }
+}
